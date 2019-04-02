ccb2c0b0cfcc55e075265ce1dca48111122f6bda
Jonathan <33239505+jonathanssm@users.noreply.github.com>
2019-04-02 20:16:28 -0300

    Add files via upload

---
+++ test/com/clinica/test/Funcionario.java
@@ -0,0 +1,78 @@
+package com.clinica.test;
+
+public class Funcionario {
+	
+	private String cpf;
+	private String nome;
+	private String endereco;
+	private int idade;
+    private int telefone;
+    private String rg;
+    private String pis;
+    private String ctps;
+	
+   
+	public Funcionario(String cpf, String nome, String endereco, int idade,int telefone,String rg, String pis,String ctps) {
+			super();
+			this.endereco = endereco;
+			this.idade = idade;
+			this.telefone = telefone;
+			this.cpf = cpf;
+			this.rg = rg;
+			this.pis = pis;
+			this.ctps = ctps;
+			this.nome = nome;
+		}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public int getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(int telefone) {
+		this.telefone = telefone;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getRg() {
+		return rg;
+	}
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	public String getPis() {
+		return pis;
+	}
+	public void setPis(String pis) {
+		this.pis = pis;
+	}
+	public String getCtps() {
+		return ctps;
+	}
+	public void setCtps(String ctps) {
+		this.ctps = ctps;
+	}
+	
+	
+
+}
---
+++ test/com/clinica/test/FuncionarioTest.java
@@ -0,0 +1,15 @@
+package com.clinica.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FuncionarioTest {
+
+	@Test
+	void test() {
+	Funcionario func = new Funcionario("02986105580","Tacyanne","Rua Itaporanga",30,32246327,"30738130","1234567","5678945");
+		fail("Not yet implemented");
+	}
+
+}
