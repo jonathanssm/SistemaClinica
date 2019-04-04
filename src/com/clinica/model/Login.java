c1c748d3bb30c432df1ddc9b08377cb1df8d671b
Jonathan <33239505+jonathanssm@users.noreply.github.com>
2019-04-04 15:37:54 -0300

    Add files via upload

---
+++ src/com/clinica/model/Login.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.clinica.model;
+
+/**
+ *
+ * @author Jonathan
+ */
+public class Login {
+
+    private Long id;
+    private String login;
+    private String senha;
+    private String tipo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+}
